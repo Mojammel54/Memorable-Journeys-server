1c49b3e18eec67e24f5bf43f47c363c1dde9ac38
JIBON <jibon2230@gmail.com>
2022-11-08 01:18:22 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const express = require('express');
 const cors = require('cors');
 
@@ -9,6 +11,43 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json())
 
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ebaxxgs.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+async function run() {
+
+    try {
+
+        const tourCollection = client.db("memorable-journeys").collection("services");
+
+    }
+
+
+    finally {
+
+
+
+
+
+    }
+
+
+
+}
+run().catch(err => {
+
+    console.log(err)
+
+
+})
 
 
 
